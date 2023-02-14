2fe471733f4472b4a29bf3b2900720cde9615c35
RYE415 <rhk415@gmail.com>
2023-02-15 00:20:06 +0900

    Modify RecommendationController

--- src/main/java/com/pre/helloworld/RecommendationController.java
+++ src/main/java/com/pre/helloworld/RecommendationController.java
@@ -8,6 +8,6 @@ public class RecommendationController {
 
     @GetMapping("/")
     public String helloWorld() {
-        return "To-do-Application !";
+        return "To-do-Application ! 수정 완료";
     }
 }
